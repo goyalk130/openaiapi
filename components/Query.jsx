b2e2f32442073d44271384a46e132f36290db100
goyalk130 <goyalkaran130@gmail.com>
2023-05-05 21:37:33 +0530

    spacing added

--- components/Query.jsx
+++ components/Query.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Query = ({query,reply}) => {
   return (
     <div className='w-full flex flex-col m-2 ml-0 bg-slate-800'>
-        <div className='flex-0 w-full flex flex-row'><h1 className='w-fit text-blue-400'>query : </h1><h1 className='flex-0'>{query}</h1></div>
-        <div className='flex-0 w-full flex flex-row'><h1 className='w-fit text-blue-400'>response : </h1><h1 className='flex-0'>{reply}</h1></div>
+        <div className='flex-0 w-full flex flex-row'><h1 className='w-fit text-blue-400'>query : </h1><h1 className='flex-0'>&nbsp;{query}</h1></div>
+        <div className='flex-0 w-full flex flex-row'><h1 className='w-fit text-blue-400'>response : </h1><h1 className='flex-0'>&nbsp;{reply}</h1></div>
     </div>
-    
+
   )
 }
 
